947e52397029eab286cdd98f4230c38a39dad68f
“Farkhad <askarovfarkhad.96@gmail.com>
2022-06-23 13:18:13 +0300

    added homework 2, 4, 5

---
+++ AskarovFarkhad-STC-22-13/src/main/java/Homework_5/Homework_5_1.java
@@ -0,0 +1,19 @@
+package Homework_5;
+
+import java.util.Scanner;
+
+public class Homework_5_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int countEven = 0, countOdd = 0, a = scanner.nextInt();
+
+        while (a != -1) {
+            if (a % 2 == 0) countEven++;
+            else countOdd++;
+
+            a = scanner.nextInt();
+        }
+        System.out.println("Количество четных чисел: " + countEven + "\n" +
+                "Количество нечетных чисел: " + countOdd);
+    }
+}
