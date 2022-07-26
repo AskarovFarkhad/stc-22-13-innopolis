e0cb62fc75c6ffec6043330195a39982405ad3ba
HaKooNaMaTaTa <igonin.oleg@otr.ru>
2022-07-26 16:19:15 +0300

    added arraylist and linkedlist

---
+++ Projects/1.18 ArrayList and LinkedList/1.18.ArrayListAndLinkedList/src/oldVersion/Main.java
@@ -0,0 +1,32 @@
+package oldVersion;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyArray myArray = new MyArray();
+
+        int i = 0;
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+        myArray.addElement(i++);
+
+        int j = 0;
+    }
+}
